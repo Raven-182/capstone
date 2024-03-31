c5c5dbb0e56726c6c876feca66b9ff100ee0c9c4
Raven G <raven@Ravens-MacBook-Air.local>
2024-03-31 18:44:58 -0400

    Initial commit:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home.js';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import Meetings from './components/pages/Meetings.js';
+import Calendars from './components/pages/Calendars.js';
+import Journal from './components/pages/Journal.js';
+import SignUp from './components/pages/SignUp.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact Component={Home}/>
+          <Route path="/calendars" exact Component={Calendars}/>
+          <Route path="/notes" exact Component={Meetings}/>
+          <Route path="/journals" exact Component={Journal}/>
+          <Route path="/sign-up" exact Component={SignUp}/>
+        </Routes>
+      </Router>
+    
+    </>
   );
 }
 
---
+++ src/components/pages/Meetings.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css'
+
+export default function Meetings(){
+    return <h1 className="meetings">Meeting notes</h1>
+}
